2cf6c0f33d3889bdaa104713301a41a0e26c2425
fridalundstroms <frida.lundstrom@yh.nackademin.se>
2023-11-23 23:17:18 +0100

    Added local pipeline

---
+++ localpipe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Creating enviroment
+
+if [ ! -d "backend/venv" ]; then
+    python -m venv backend/venv
+fi
+
+#Activating enviroment
+source backend/venv/Scripts/activate
+
+# Installning requirements.txt
+pip install -r backend/requirements.txt
+
+# Running Pylint
+python -m pylint --fail-under=9 --recursive true --ignore=venv backend
+
+# Running Black
+black backend/
+
+# Build docker image ping-url .
+docker build -t ping-url .
+
+# Run docker container
+CONTAINER_ID=$(docker run --network=host -d ping-url)
+
+# Docker - stop and remove container.
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
+
+echo "Working"
