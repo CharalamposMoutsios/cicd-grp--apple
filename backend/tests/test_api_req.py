3f3909999c8201a811214095b6a12375640c4cbd
Tolli-txt <tolli.txt@gmail.com>
2023-11-27 20:51:24 +0100

    Added some tests

---
+++ backend/tests/test_api_req.py
@@ -0,0 +1,18 @@
+import requests
+
+
+API_URL = "http://localhost:5000"
+
+
+def test_set_watched_url():
+    to_post = {
+        "activateAt": "2023-11-06T01:36:28.610Z",
+        "force": True,
+        "periodSec": 30,
+        "url": "https://google.com",
+    }
+    assert requests.post(f"{API_URL}/watched-urls", json=to_post, timeout=20).json() == {
+        "message": "Watched URL added",
+        "urlId": 0,
+    }
+
