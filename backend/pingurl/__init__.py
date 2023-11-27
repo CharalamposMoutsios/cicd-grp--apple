c33917a7122df28dd9526b7c0161a990b993e077
fridalundstroms <frida.lundstrom@yh.nackademin.se>
2023-11-27 17:11:56 +0100

    revert

--- backend/pingurl/__init__.py
+++ backend/pingurl/__init__.py
@@ -1,11 +1,12 @@
-import atexit
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
-from pingurl import watched_urls
+import atexit
 
 app = Flask(__name__)
 scheduler = BackgroundScheduler(daemon=True)
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
-atexit.register(scheduler.shutdown())
+atexit.register(lambda: scheduler.shutdown())
+
+from pingurl import watched_urls
