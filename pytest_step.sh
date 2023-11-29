aec2c2317d4033df60bcc8d39dad6acf663ebabb
Tolli-txt <tolli.txt@gmail.com>
2023-11-29 11:54:58 +0100

    Commented out first test to try other tests

--- pytest_step.sh
+++ pytest_step.sh
@@ -10,8 +10,8 @@ source venv/bin/activate
 
 pip install -r backend/requirements.txt
 
-echo "Running test_api_req.py..."
-pytest ./backend/tests/test_api_req.py
+#echo "Running test_api_req.py..."
+#pytest ./backend/tests/test_api_req.py
 
 echo "Running other tests..."
 pytest ./backend/tests/test_*
