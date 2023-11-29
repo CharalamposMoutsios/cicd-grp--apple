b1e1afb8eaa399a8970c1ead912fa68d06eb07d9
Isac Grive <isac.grive@gmail.com>
2023-11-29 12:00:45 +0100

    More tests

--- backend/tests/test_persistance.py
+++ backend/tests/test_persistance.py
@@ -35,3 +35,15 @@ def test_get_watched_url():
     url_id = add_watched_url(watched_url)
     retrieved_url = get_watched_url(url_id)
     assert retrieved_url == watched_url
+
+def test_delete_watched_url():
+    watched_url = WatchedUrl(
+        activate_at=datetime.now(),
+        force=True,
+        url="http://example.com",
+        period_sec=60,
+        url_id=None)
+    url_id = add_watched_url(watched_url)
+    delete_watched_url(url_id)
+    with pytest.raises(WatchedUrlNotFoundError):
+        get_watched_url(url_id)
