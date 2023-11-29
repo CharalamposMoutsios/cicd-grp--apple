409258098860d13a0b467b86ec1b372478723aca
Isac Grive <isac.grive@gmail.com>
2023-11-29 11:54:24 +0100

    Added more persistance test

--- backend/tests/test_persistance.py
+++ backend/tests/test_persistance.py
@@ -25,3 +25,13 @@ def test_add_watched_url():
     assert url_id is not None
     assert url_id in get_url_ids()
 
+def test_get_watched_url():
+    watched_url = WatchedUrl(
+        activate_at=datetime.now(),
+        force=True,
+        url="http://example.com",
+        period_sec=60,
+        url_id=None)
+    url_id = add_watched_url(watched_url)
+    retrieved_url = get_watched_url(url_id)
+    assert retrieved_url == watched_url
