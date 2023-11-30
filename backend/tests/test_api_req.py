5049cbb81d7835444126d436bdfc8bbaecfb9e44
Tolli-txt <tolli.txt@gmail.com>
2023-11-30 02:12:10 +0100

    Changed assert output to avoid conlfict with other test. Co-contributor: Charalampos

--- backend/tests/test_api_req.py
+++ backend/tests/test_api_req.py
@@ -10,10 +10,10 @@ def test_set_watched_url():
         "activateAt": "2023-11-06T01:36:28.610Z",
         "force": True,
         "periodSec": 30,
-        "url": "https://google.com",
+        "url": "https://klockren.nu",
     }
     assert requests.post(f"{API_URL}/watched-urls", json=to_post, timeout=20).json() == {
         "message": "Watched URL added",
-        "urlId": 0,
+        "urlId": 1,
     }
 
