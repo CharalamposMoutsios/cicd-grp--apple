00756dd835022742321aa9aac86745f4fcb22555
Tolli-txt <tolli.txt@gmail.com>
2023-11-30 02:25:16 +0100

    Remade file in hopes of getting pytest to not complain

--- backend/tests/test_api_req.py
+++ backend/tests/test_api_req.py
@@ -1,9 +1,7 @@
 import os
 import requests
 
-
 API_URL = f"http://{os.environ['CONTAINER_IP']}:5000"
-
 
 def test_set_watched_url():
     to_post = {
@@ -12,8 +10,8 @@ def test_set_watched_url():
         "periodSec": 30,
         "url": "https://klockren.nu",
     }
+
     assert requests.post(f"{API_URL}/watched-urls", json=to_post, timeout=20).json() == {
         "message": "Watched URL added",
         "urlId": 3,
     }
-
