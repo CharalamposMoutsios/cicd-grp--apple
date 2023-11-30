8e6f882415069a62e87bd8c05f3132335d69e014
fridalundstroms <frida.lundstrom@yh.nackademin.se>
2023-11-30 09:40:05 +0100

    comments

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from pingurl.models import WatchedUrl, PingData
 import pytest
 
+# Skapar ett testobjekt av WatchedUrl.
 @pytest.fixture
 def wu_fixture():
     return WatchedUrl(
@@ -13,6 +14,7 @@ def wu_fixture():
         url_id=1,
     )
 
+# Skapar ett testobjekt av PingData.
 @pytest.fixture
 def ping_data_fixture():
     return PingData(
@@ -22,6 +24,7 @@ def ping_data_fixture():
         url_id=1,
     )
 
+# Test för att testa skapandet av WatchedUrl objekt.
 @patch("pingurl.models.WatchedUrl", MagicMock)
 def test_wu_creation(wu_fixture):
     assert wu_fixture.activate_at == datetime(2023, 1, 1)
@@ -30,37 +33,13 @@ def test_wu_creation(wu_fixture):
     assert wu_fixture.url == "https://example.com"
     assert wu_fixture.url_id == 1
 
-@patch("pingurl.models.WatchedUrl", MagicMock)
-def test_wu_to_dict(wu_fixture):
-    expected_dict = {
-        "activateAt": "2023-01-01T00:00:00",
-        "force": True,
-        "periodSec": 60,
-        "url": "https://example.com",
-        "urlId": 1,
-    }
-    assert wu_fixture.to_dict() == expected_dict
-
+# Test för att testa skapandet av PingData-Objekt.
 @patch("pingurl.models.PingData", MagicMock)
 def test_ping_data_creation(ping_data_fixture):
     assert ping_data_fixture.pinged_at == datetime(2023, 1, 1, 12, 0, 0)
     assert ping_data_fixture.response_time_sec == timedelta(seconds=1)
     assert ping_data_fixture.status_code == 200
     assert ping_data_fixture.url_id == 1
-
-@patch("pingurl.models.PingData", MagicMock)
-def test_ping_data_to_dict(ping_data_fixture):
-    expected_dict = {
-        "urlId": 1,
-        "pingedAt": "2023-01-01T12:00:00",
-        "responseTimeSec": 1.0,
-        "statusCode": 200,
-    }
-    assert ping_data_fixture.to_dict() == expected_dict
-
-@patch("pingurl.models.PingData", MagicMock)
-def test_ping_data_ok(ping_data_fixture):
-    assert ping_data_fixture.ok()
 
 @patch("pingurl.models.validators.url", return_value=False)
 def test_invalid_url(mock_validators_url):
