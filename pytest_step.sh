f320508a7d530e81802cf2c17dc4d4e88aa95f02
Tolli-txt <tolli.txt@gmail.com>
2023-11-30 01:20:54 +0100

    Added get_container_ip.sh execution, added comments

--- pytest_step.sh
+++ pytest_step.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Debugging
 hostname
-
 echo "Shell: $SHELL"
 
+# Running get_container_ip.sh to get the CONTAINER_IP variable
+# used by test_api_req.py
+source get_container_ip.sh
+
+# Creating venv
 python3 -m venv venv
 
+# Activating venv
 source venv/bin/activate
 
+# Installing requirements.txt
 pip install -r backend/requirements.txt
 
-#echo "Running test_api_req.py..."
-#pytest ./backend/tests/test_api_req.py
+echo "Running test_api_req.py..."
+pytest ./backend/tests/test_api_req.py
 
 echo "Running other tests..."
 pytest ./backend/tests/test_*
